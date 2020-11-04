bc5ae2ffa709aba153d89c62d3bfbc955a0268c9
Manan Brahmbhatt <manan@Manans-MBP.hsd1.il.comcast.net>
2020-11-03 19:40:04 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+st.title("Kumar Brahmbhatt")
+st.write("[LinkedIn](http://linkedin.com/in/kumarbrahmbhatt)")
+st.header("Online Resume")
+
+# Executive Summary
+st.subheader("Executive Summary")
+st.write("* Data Scientist with 4 years of experience in Financial Technology, Banking, and Healthcare Insurance.") 
+st.write("* Focus on fast analytics at scale - measurable success from revenue generated or cost savings by campaigns.")
+st.write("* Specialty in marketing analytics, consumer behavior research, and experimental design.")
+
+st.subheader("Education")
+st.write("University of Illinois at Chicago, Master of Science in Business Analytics (2016-2017)")
+st.write("University of Illinois at Urbana-Champaign, Bachelor of Arts in Economics (2012-2016)")
+st.write("University of Illinois at Urbana-Champaign, Grainger College of Engineering - Technology Commercialization (2016)")
+
+
+st.subheader("Experience")
+st.write("Health Care Services Corporation, Chicago — Associate Data Scientist (MAY 2020 - PRESENT)")
+st.write("BMO Harris Bank, Chicago — Data Scientist (JUN 2019 - MAY 2020)")
+st.write("NextCapital, Chicago — Data Scientist (JAN 2017 - JUN 2019)")
+
+st.subheader("Projects")
+st.write("Coming soon!")
+
+st.subheader("Writing Sample")
+st.write("Coming soon!")
